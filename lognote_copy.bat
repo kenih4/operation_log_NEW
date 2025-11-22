1366ef678320b0301aa064ea19aef9408dc5deef
Kenichi Ishii <kenih4@gmail.com>
2025-11-23 08:22:55 +0900

    コメント追記

---
+++ lognote_copy.bat
@@ -0,0 +1,16 @@
+
+rem     GitBash     .\test.bat
+
+
+copy \\saclaoprfs01.spring8.or.jp\log_note\SP8\operation_log\2022\01\2022_01.xlsm C:\Users\kenic\Documents\operation_log_NEW\SP8\2022_01_SP8.xlsm
+copy \\saclaoprfs01.spring8.or.jp\log_note\SP8\operation_log\2022\02\2022_02.xlsm C:\Users\kenic\Documents\operation_log_NEW\SP8\2022_02_SP8.xlsm
+copy \\saclaoprfs01.spring8.or.jp\log_note\SP8\operation_log\2022\03\2022_03.xlsm C:\Users\kenic\Documents\operation_log_NEW\SP8\2022_03_SP8.xlsm
+copy \\saclaoprfs01.spring8.or.jp\log_note\SP8\operation_log\2022\04\2022_04.xlsm C:\Users\kenic\Documents\operation_log_NEW\SP8\2022_04_SP8.xlsm
+copy \\saclaoprfs01.spring8.or.jp\log_note\SP8\operation_log\2022\05\2022_05.xlsm C:\Users\kenic\Documents\operation_log_NEW\SP8\2022_05_SP8.xlsm
+copy \\saclaoprfs01.spring8.or.jp\log_note\SP8\operation_log\2022\06\2022_06.xlsm C:\Users\kenic\Documents\operation_log_NEW\SP8\2022_06_SP8.xlsm
+copy \\saclaoprfs01.spring8.or.jp\log_note\SP8\operation_log\2022\07\2022_07.xlsm C:\Users\kenic\Documents\operation_log_NEW\SP8\2022_07_SP8.xlsm
+copy \\saclaoprfs01.spring8.or.jp\log_note\SP8\operation_log\2022\08\2022_08.xlsm C:\Users\kenic\Documents\operation_log_NEW\SP8\2022_08_SP8.xlsm
+copy \\saclaoprfs01.spring8.or.jp\log_note\SP8\operation_log\2022\09\2022_09.xlsm C:\Users\kenic\Documents\operation_log_NEW\SP8\2022_09_SP8.xlsm
+copy \\saclaoprfs01.spring8.or.jp\log_note\SP8\operation_log\2022\10\2022_10.xlsm C:\Users\kenic\Documents\operation_log_NEW\SP8\2022_10_SP8.xlsm
+copy \\saclaoprfs01.spring8.or.jp\log_note\SP8\operation_log\2022\11\2022_11.xlsm C:\Users\kenic\Documents\operation_log_NEW\SP8\2022_11_SP8.xlsm
+copy \\saclaoprfs01.spring8.or.jp\log_note\SP8\operation_log\2022\12\2022_12.xlsm C:\Users\kenic\Documents\operation_log_NEW\SP8\2022_12_SP8.xlsm
